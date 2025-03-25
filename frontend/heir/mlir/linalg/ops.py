3a9849baca66e59bce7971a107f70bd79a16717d
Alexander Viand <alexander.viand@intel.com>
2025-03-25 15:39:08 -0700

    notes

--- frontend/heir/mlir/linalg/ops.py
+++ frontend/heir/mlir/linalg/ops.py
@@ -20,3 +20,15 @@ def matmul(
     rhs: Secret[Tensor] | Tensor | NDArray,
 ) -> Tensor:
   return np.matmul(lhs, rhs)  # type: ignore
+
+
+# We want to have the function body above (numpy matmul) called in func.original
+# and we want to emit `linalg.matmul` when running foo()/foo.eval()!
+# Ideally, we'd also like to have uses of matmul outside of @compile() still
+# correctly forward to np.matmul...
+
+# @compile()
+# def foo(
+#     a: Secret[Tensor[32, 32, F32]], b: Secret[Tensor[32, 32, F32]]
+# ) -> Secret[Tensor[32, 32, F32]]:
+#   return matmul(a, b)
