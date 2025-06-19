813ac34fd89a09c43024dbaad0c4f43be1ac58ce
Alexander Viand <alexander.viand@gmail.com>
2025-06-18 17:24:58 -0700

    SecretTypes: add getTypeOrValueType helper

--- lib/Dialect/Secret/IR/SecretTypes.h
+++ lib/Dialect/Secret/IR/SecretTypes.h
@@ -2,8 +2,24 @@
 #define LIB_DIALECT_SECRET_IR_SECRETTYPES_H_
 
 #include "lib/Dialect/Secret/IR/SecretDialect.h"
+#include "llvm/include/llvm/Support/Casting.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Types.h"         // from @llvm-project
 
 #define GET_TYPEDEF_CLASSES
 #include "lib/Dialect/Secret/IR/SecretTypes.h.inc"
+
+namespace mlir {
+namespace heir {
+namespace secret {
+
+inline Type getTypeOrValueType(Type ty) {
+  if (auto secretTy = dyn_cast<secret::SecretType>(ty))
+    return secretTy.getValueType();
+  return ty;
+}
+
+}  // namespace secret
+}  // namespace heir
+}  // namespace mlir
 
 #endif  // LIB_DIALECT_SECRET_IR_SECRETTYPES_H_
